cc2f880fa6863d69bd889a4e8c0ee60ed6bfcb91
petres <peter.reschenhofer@gmail.com>
2015-12-15 19:17:24 +0100

    splitting up files

--- main.py
+++ main.py
@@ -1,152 +1,60 @@
 #!/bin/python2
 import webcolors as wc
+from signs import digit_signs, separator_types
+from settings import digit_leds, separator_leds
 
 spidevBuffer = "/dev/spidev0.0"
-off = bytearray(b'\x00\x00\x00')
 
-##################
-# LED NUMERATION 
-##################
-digit = {} 
-
-digit[0] = [ 0, 1, 2, 3, 4, 5, 6]
-digit[1] = [ 7, 8, 9,10,11,12,13]
-digit[2] = [16,17,18,19,20,21,22]
-digit[3] = [23,24,25,26,27,28,29]
-
-point    = [14,15]
-
-
-
-
-##################
-# LED NUMERATION | SEVEN SEGMENT
-##################
-#
-#     0000
-#    5    1   00
-#    5    1   00
-#     6666 
-#    4    2   11
-#    4    2   11
-#     3333 
-#
-##################
-
-sign = {}
-##################
-# DIGITS
-##################
-sign["0"] = [0,1,2,3,4,5]
-sign["1"] = [1,2]
-sign["2"] = [0,1,3,4,6]
-sign["3"] = [0,1,2,3,6]
-sign["4"] = [1,2,5,6]
-sign["5"] = [0,2,3,5,6]
-sign["6"] = [2,3,4,5,6]
-sign["7"] = [0,1,2]
-sign["8"] = range(7)
-sign["9"] = [0,1,2,3,5,6]
-
-##################
-# LETTERS
-##################
-sign["A"] = [0,1,2,4,5,6]
-sign["B"] = range(7)
-sign["b"] = [2,3,4,5,6]
-sign["C"] = [0,3,4,5]
-sign["c"] = [3,4,6]
-sign["d"] = [1,2,3,4,6]
-sign["E"] = [0,3,4,5,6]
-sign["F"] = [0,4,5,6]
-sign["G"] = [0,2,3,4,5]
-sign["H"] = [1,2,4,5,6]
-sign["h"] = [2,4,5,6]
-sign["I"] = sign["1"]
-sign["J"] = [0,1,2,3,4]
-# sign["K"]
-sign["L"] = [3,4,5]
-# sign["M"]
-# sign["N"]
-sign["O"] = sign["0"]
-sign["o"] = [2,3,4,6]
-sign["P"] = [0,1,4,5,6]
-# sign["Q"]
-# sign["R"]
-sign["S"] = sign["5"]
-# sign["T"]
-sign["U"] = [1,2,3,4,5,6]
-#sign["V"]
-#sign["W"]
-#sign["X"]
-sign["Y"] = [1,2,3,5,6]
-sign["Z"] = sign["2"]
-
-##################
-# SPECIAL CHARS
-##################
-sign["_"] = [3]
-sign["-"] = [6]
-
-
-
-
-##################
-# POINTS (all combinations)
-##################
-points = {}
-points[0] = []
-points[1] = [0]
-points[2] = [1]
-points[3] = [0,1]
-
-
-
-
-spidev = open(spidevBuffer,"wb")
-
+#spidev = open(spidevBuffer,"wb")
 
 def _get_color(color):
     if isinstance(color, basestring):
         if color.startswith("#"):
             if len(color) == 4:
                 a = (int(color[1]*2, 16), int(color[2]*2, 16), int(color[3]*2, 16))
-            elif en(color) == 7:
-                a = (int(color[1:2], 16), int(color[3:4]*2, 16), int(color[5:6]*2, 16))
+            elif len(color) == 7:
+                a = (int(color[1:3], 16), int(color[3:5], 16), int(color[5:7], 16))
         else:
-            a = name_to_rgb(color)
-    
+            a = wc.name_to_rgb(color)
+            
     return bytearray(chr(a[2]) + chr(a[1]) + chr(a[0]))
 
 def write_time(time, color):
     pass
 
 def get_leds(ascii, position):
-    if ascii in sign:
+    if ascii in digit_signs:
         ret = []
-        #ret = [digit[position][l] for l in sign[ascii]]
-        for l in sign[ascii]:
-            ret.append(digit[position][l])
+        #ret = [digitLeds[position][l] for l in sign[ascii]]
+        for l in digit_signs[ascii]:
+            ret.append(digit_leds[position][l])
         return ret
     else:
         raise Exception("Sign not implemented")
 
-def write(string, delimiter=0, color = "white"):
-    temp = []
+def write(string, separator="OFF", color = "white", off_color = "black"):
+    leds = []
     for i, c in enumerate(string):
-        temp += get_leds(c, i)
-    for i in points[delimiter]:
-        temp += point[i]
+        leds += get_leds(c, i)
+    for i in separator_types[separator]:
+        leds += separator_leds[i]
 
-    byte_color = _get_color(color)
+    color_bytes = _get_color(color)
+    off_color_bytes = _get_color(off_color)
 
-    writeString = ''
+    message_bytes = ''
     for i in range(30):
-        if i in temp:
-            writeString += byte_color
+        if i in leds:
+            message_bytes += color_bytes
         else:
-            writeString += off
-    spidev.write(writeString)
-    spidev.flush()
+            message_bytes += off_color_bytes
+
+    #spidev.write(writeString)
+    #spidev.flush()
 
 
+print(_get_color("white"))
+print(_get_color("#fff"))
+print(_get_color("#ffffff"))
+
+write('0000')
---
+++ signs.py
@@ -0,0 +1,82 @@
+####################################
+# LED NUMERATION | SEVEN SEGMENT
+####################################
+#
+#     0000
+#    5    1   00
+#    5    1   00
+#     6666 
+#    4    2   11
+#    4    2   11
+#     3333 
+#
+##################
+
+digit_signs = {}
+##################
+# DIGITS
+##################
+digit_signs["0"] = [0,1,2,3,4,5]
+digit_signs["1"] = [1,2]
+digit_signs["2"] = [0,1,3,4,6]
+digit_signs["3"] = [0,1,2,3,6]
+digit_signs["4"] = [1,2,5,6]
+digit_signs["5"] = [0,2,3,5,6]
+digit_signs["6"] = [2,3,4,5,6]
+digit_signs["7"] = [0,1,2]
+digit_signs["8"] = range(7)
+digit_signs["9"] = [0,1,2,3,5,6]
+
+##################
+# LETTERS
+##################
+digit_signs["A"] = [0,1,2,4,5,6]
+digit_signs["B"] = range(7)
+digit_signs["b"] = [2,3,4,5,6]
+digit_signs["C"] = [0,3,4,5]
+digit_signs["c"] = [3,4,6]
+digit_signs["d"] = [1,2,3,4,6]
+digit_signs["E"] = [0,3,4,5,6]
+digit_signs["F"] = [0,4,5,6]
+digit_signs["G"] = [0,2,3,4,5]
+digit_signs["H"] = [1,2,4,5,6]
+digit_signs["h"] = [2,4,5,6]
+digit_signs["I"] = digit_signs["1"]
+digit_signs["J"] = [0,1,2,3,4]
+# digit_signs["K"]
+digit_signs["L"] = [3,4,5]
+# digit_signs["M"]
+# digit_signs["N"]
+digit_signs["O"] = digit_signs["0"]
+digit_signs["o"] = [2,3,4,6]
+digit_signs["P"] = [0,1,4,5,6]
+# digit_signs["Q"]
+# digit_signs["R"]
+digit_signs["S"] = digit_signs["5"]
+# digit_signs["T"]
+digit_signs["U"] = [1,2,3,4,5,6]
+#digit_signs["V"]
+#digit_signs["W"]
+#digit_signs["X"]
+digit_signs["Y"] = [1,2,3,5,6]
+digit_signs["Z"] = digit_signs["2"]
+
+##################
+# SPECIAL CHARS
+##################
+digit_signs["_"] = [3]
+digit_signs["-"] = [6]
+
+
+
+##################
+# POINTS (all combinations)
+##################
+separator_types = {}
+separator_types['OFF']    = []
+separator_types['LOWER']  = [0]
+separator_types['UPPER']  = [1]
+separator_types['BOTH']   = [0,1]
+
+
+
