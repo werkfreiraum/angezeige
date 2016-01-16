daa89cbce3140d4347dd614111a1ebb191457fec
petres <peter.reschenhofer@gmail.com>
2016-01-16 19:55:38 +0100

    time local fix, more signs added

--- plugins/time_progs.py
+++ plugins/time_progs.py
@@ -1,5 +1,5 @@
 from programs import Program, promoteProgram
-from time import gmtime, strftime
+from time import localtime, strftime
 
 ################################################################
 # Shows current time
@@ -9,8 +9,8 @@ class ShowTime(Program):
     def do(self):
         separator = ["INNER", "NONE"]
         while True:
-            signs = strftime("%H%M", gmtime())
-            sec = int(strftime("%S", gmtime()))
+            signs = strftime("%H%M", localtime())
+            sec = int(strftime("%S", localtime()))
             self.write(signs, separator = separator[sec%len(separator)])
             # FIXME displayed clock will be wrong by design (up to 100ms)
             self.wait(0.1)
