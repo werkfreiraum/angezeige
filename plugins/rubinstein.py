2fdbd84948f15d45dabfa6abd0578032326b3a3f
petres <peter.reschenhofer@gmail.com>
2016-01-23 00:23:35 +0100

    bugfixes

--- plugins/rubinstein.py
+++ plugins/rubinstein.py
@@ -29,7 +29,6 @@ class RubinsteinProgress(JsonReader):
 
     def getMessage(self):
         message = int(round(JsonReader.getMessage(self)))
-        #message = 100
         if message == 100:
             return "finished!"
         else:
@@ -37,27 +36,4 @@ class RubinsteinProgress(JsonReader):
 
     @staticmethod
     def getParams():
-        return {}
-
-
-################################################################
-# Show when printer rubinstein ready
-################################################################
-# class RubinsteinProgress(Program):
-#     def do(self):
-#         while True:
-#             for key, param_paths in PARAMTERS.iteritems():
-#                 for param_path in param_paths:
-#                     api_uri = "{}{}?apikey={}".format(URL, key, api_key)
-#                     try:
-#                         # need contextlib because no Python 3... :(
-#                         with closing(urllib2.urlopen(api_uri)) as params_json:
-#                             params = json.load(params_json)
-#                     except urllib2.URLError:
-#                         logging.exception("Failed to retrieve rubinstein"
-#                                           "data.")
-#                         self.write('')
-#                     else:
-#                         param = int(round(_get_val_by_path(params, param_path)))
-#                         self.write(str(param))
-#                     self.wait(CYCLE_PERIOD_S)
+        return Program.getParams()
