ff7949d69b9ee6aeabe328a98a59f64c703877a1
petres <peter.reschenhofer@gmail.com>
2016-07-23 22:42:35 +0200

    adding foosball direct

--- plugins/foosball_progs.py
+++ plugins/foosball_progs.py
@@ -7,7 +7,7 @@ from url_progs import UrlReader, JsonReader
 URI_FOOSBALL = "http://foosball.local:5000"
 
 
-class SimpleFoosball(UrlReader):
+class WebServerFoosball(UrlReader):
 
     def __init__(self, uri=None):
         UrlReader.__init__(self, uri=uri, refresh_duration=0)
@@ -35,26 +35,48 @@ class TestSimpleFoosball(Program):
         params['uri'] = URI_FOOSBALL
         return params
 
-# class ColorFoosball(UrlReader):
 
-#     def __init__(self, uri=None):
-#         UrlReader.__init__(self, uri=uri, refresh_duration=0)
+class DirectFoosball(Program):
 
-#     @staticmethod
-#     def get_params():
-#         params = {}
-#         params['uri'] = URI_FOOSBALL + "/color"
-#         return params
+    pin_red = 14
+    pin_blue = 15
 
-#     def do(self):
-#         while True:
+    pin_reset = 18
 
-#             d_info = json.loads(self.readUri())
-#             JsonReader.get_val_by_path(j_dct, "color")
+    bounce_time = 2000
 
-#              = self.getMessage()
+    score = [0, 0]
 
+    def __init__(self):
+        Program.__init__(self)
+        global GPIO
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin_red, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(pin_blue, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(pin_reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-#             return JsonReader.get_val_by_path(j_dct, path)
-#                 self.write(message)
-#             self.wait(self.refresh_duration, show_progress=False)
+        GPIO.add_event_detect(pin_red, GPIO.FALLING, callback=self.goal, bouncetime=self.bounce_time)
+        GPIO.add_event_detect(pin_blue, GPIO.FALLING, callback=self.goal, bouncetime=self.bounce_time)
+
+        GPIO.add_event_detect(pin_reset, GPIO.FALLING, callback=self.reset, bouncetime=self.bounce_time)
+
+    def goal(self, channel):
+        logging.debug("GOAL" + str(channel))
+        self.update = True
+        if channel == self.pin_red:
+            self.score[0] += 1
+        if channel == self.pin_blue:
+            self.score[1] += 1
+
+    def do(self):
+        while True:
+            self.write('{:2}{:2}'.format(*score))
+            self.wait(0)
+
+    def resetGoals(channel):
+        self.score = [0, 0]
+
+    @staticmethod
+    def get_params():
+        return {}
