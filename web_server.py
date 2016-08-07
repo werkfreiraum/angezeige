e26d93b5e2cf2f9292613d07048f1d337b930d2e
petres <peter.reschenhofer@gmail.com>
2016-08-07 15:37:58 +0200

    web server serves

--- web_server.py
+++ web_server.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='web', static_url_path='')
 
-@app.route("/show")
-def show():
-    return "P" + str(GPIO.input(14)) + str(GPIO.input(15)) + str(GPIO.input(18))
+
+@app.route("/manager")
+def manager():
+    return app.send_static_file('manager.html')
+
+
+@app.route("/")
+@app.route("/reader")
+def reader():
+    return app.send_static_file('reader.html')
+
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0')
+    app.run(host='0.0.0.0')
